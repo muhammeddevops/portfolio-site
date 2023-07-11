3beed38a5c926cf6b0d2a0132e4a1741a6828ccf
Muhammed <muhammednabeel@live.co.uk>
2023-07-11 12:03:33 +0100

    begins work on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          {/* vertical line is below and circle is above */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Muhammed 👨‍💻</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop Full Stack JavaScript, <br className="sm:block hidden" />{" "}
+            web and mobile, applications
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
