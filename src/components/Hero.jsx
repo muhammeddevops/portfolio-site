9110cdc54b0e025e93c3f3ebaee559609879ff87
Muhammed <muhammednabeel@live.co.uk>
2023-07-19 23:42:49 +0100

    fixes bg on home pg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     // Im not sure why or how but having the section below and using classname h-screen provides the dark blue with curv lines bg
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto bg-hero-pattern bg-cover">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
