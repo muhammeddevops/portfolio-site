1b33062c43b6e50649b4e97fbd2c69df16841286
Muhammed <muhammednabeel@live.co.uk>
2023-07-19 21:29:13 +0100

    modified 3d model and restructured app

---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Contact, StarsCanvas } from "./index";
+
+const ContactPage = () => {
+  return (
+    <div className="relative z-0 min-h-screen">
+      <Contact />
+      <StarsCanvas />
+    </div>
+  );
+};
+
+export default ContactPage;
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,27 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./earth/earth.glb");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={5}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} position={[-20, -10, 10]} />
+      <primitive
+        object={earth.scene}
+        scale={0.2}
+        position-y={0}
+        rotation-y={0}
+      />
+    </mesh>
   );
 };
 
