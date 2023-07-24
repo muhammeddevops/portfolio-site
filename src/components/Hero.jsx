57f24d86b7af9135bd67b30cfd8c44106184e956
Muhammed <muhammednabeel@live.co.uk>
2023-07-24 15:22:43 +0100

    update text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Muhammed 👨‍💻</span>
+            Hi, I'm <span className="text-[#915eff]">Muhammed</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop Full Stack JavaScript, <br className="sm:block hidden" />{" "}
