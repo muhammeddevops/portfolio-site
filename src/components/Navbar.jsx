57a9e7c090e00c9a0edfcd7ccc93c7c08d910662
Muhammed <muhammednabeel@live.co.uk>
2023-07-28 18:39:10 +0100

    adds cv download option and readjusts navbar to fit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,30 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
+import { cv } from "../assets";
+
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+
+  const handleDownloadCV = async () => {
+    console.log("in here");
+    try {
+      const response = await fetch("/cvNew.docx"); // Replace with the correct path to your CV file
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "JSbyMKdotComCV.docx"; // Replace with your desired file name
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading the CV:", error);
+    }
+  };
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -25,27 +46,32 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Muhammed Khan &nbsp;
-            <span className="sm:block hidden">| JS Developer</span>
+            <span className="xl:block hidden">| JS Developer</span>
           </p>
         </Link>
 
-        <div className="flex gap-3 ml-3">
+        <div className=" gap-3 ml-3 flex justify-self-center">
           <a
-            className=""
+            className="mr-2 sm:mr-5 pt-3"
             href="https://www.linkedin.com/in/muhammed-khan-63b009234/"
             target="_blank"
           >
             <FaLinkedin size={31} color="#0A66C2" />
           </a>
+
+          <button onClick={handleDownloadCV}>
+            <img src={cv} alt="cv-icon" className="max-w-[35px] mr-2 sm:mr-5" />
+          </button>
+
           <a
-            className=""
+            className="pt-3"
             href="https://github.com/muhammeddevops"
             target="_blank"
           >
             <FaGithub size={30} />
           </a>
         </div>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
+        <ul className="list-none hidden lg:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -59,7 +85,7 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="lg:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
             alt="menu"
@@ -71,7 +97,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl sm:hidden`}
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl lg:hidden`}
         >
           <ul className="list-none flex  justify-end items-start flex-col gap-4">
             {navLinks.map((link) => (
