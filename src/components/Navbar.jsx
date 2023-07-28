81c997b52188a035b7c883deec954aac953ab7f7
Muhammed <muhammednabeel@live.co.uk>
2023-07-28 19:40:54 +0100

    fixes bug in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+          <p className="hidden sm:block text-white text-[18px] font-bold cursor-pointer flex">
             Muhammed Khan &nbsp;
             <span className="xl:block hidden">| JS Developer</span>
           </p>
